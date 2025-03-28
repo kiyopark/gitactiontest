7f3586064eaf91d43055bfc38ae27e89bb97c441
Lian Park <woori01@gmail.com>
2025-03-29 03:37:47 +0900

    add github

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gitactiontest',
-    version='0.0.1',
+    version='{{VERSION_PLACEHOLDER}}',
     description='git action test',
     author='Lian Park',
     author_email='g1000white@gmail.com',
